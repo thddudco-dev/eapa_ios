65561098a64edd1205cbda3d4c15c02927e6970c
송영채 <thddudco.dev@gmail.com>
2023-01-09 09:22:23 +0900

    인트로뷰 개발.

---
+++ EAPA/Common/Extensions/Color+Extension.swift
@@ -0,0 +1,41 @@
+//
+//  Color+Extension.swift
+//  EAPA
+//
+//  Created by 송영채 on 2023/01/09.
+//
+
+import Foundation
+import SwiftUI
+
+extension Color {
+    
+    // PRIMARY
+    static let COLOR_54B6AB = Color("54B6AB")
+    static let COLOR_ECFAFA = Color("ECFAFA")
+    
+    // ACCENT
+    static let COLOR_84D4E4 = Color("84D4E4")
+    static let COLOR_DCF5F9 = Color("DCF5F9")
+    
+    // NEUTRAL
+    static let COLOR_262C3D = Color("262C3D")
+    static let COLOR_747F9E = Color("747F9E")
+    static let COLOR_B0B3C7 = Color("B0B3C7")
+    static let COLOR_F8F8F8 = Color("F8F8F8")
+    static let COLOR_FFFFFF = Color("FFFFFF")
+    
+    // SEMANTIC
+    static let COLOR_FF5B5B = Color("FF5B5B")
+    static let COLOR_FFE0E0 = Color("FFE0E0")
+    static let COLOR_75D624 = Color("75D624")
+    static let COLOR_FCDF25 = Color("FCDF25")
+    
+    static let COLOR_0FB8AB = Color("0FB8AB")
+    static let COLOR_C5C5C6 = Color("C5C5C6") // TextField Placeholder
+    
+    // Shadow
+    static let COLOR_Shadow = Color.init(red: 0/255, green: 0/255, blue: 0/255, opacity: 0.3)
+    
+    
+}
--- EAPA/Views/Intro/IntroView.swift
+++ EAPA/Views/Intro/IntroView.swift
@@ -9,32 +9,43 @@ import SwiftUI
 
 struct IntroView: View {
     
+    @State private var animate = false
+    
     var body: some View {
         ZStack {
             Color.COLOR_54B6AB
                 .ignoresSafeArea()
             
-            
             Text("EAPA")
                 .foregroundColor(Color.COLOR_FFFFFF)
                 .font(.system(size: 100, weight: .heavy))
-                .opacity(logoTextOpacity)
+                .opacity(animate ? 1.0 : 0.0)
                 .padding(.bottom, 100)
+                .animation(.easeOut(duration: 0.6), value: animate)
             
             VStack {
                 Spacer()
                 Text("JUNU.COM")
                     .foregroundColor(Color.COLOR_FFFFFF)
                     .font(.system(size: 14, weight: .heavy))
-                    .opacity(copyrightTextOpacity)
-                    .onChange(of: logoTextOpacity) { _ in
-                        withAnimation(.easeInOut(duration: 1).delay(2.0)) {
-                            copyrightTextOpacity = 1.0
-                        }
-                    }
+                    .opacity(animate ? 1.0 : 0.0)
+                    .animation(.easeOut(duration: 1.0).delay(0.3), value: animate)
             }
             .padding(.bottom, 20)
         }
+        .onAppear {
+            animate = true
+        }
+        .onChange(of: animate) { _ in
+            // Guide 화면으로 갈지 메인 화면으로 갈지 구분
+            DispatchQueue.main.asyncAfter(deadline: .now() + 2.0) {
+                startService()
+            }
+        }
+    }
+    
+    func startService() {
+        print("Start Service")
     }
     
 }
